6681de540a9ef78fc8bc8067fd6a8c76406fdf2e
alejomartin2001 <amartin400@comunidad.unnoba.edu.ar>
2024-09-16 18:40:52 -0300

    first commit

---
+++ grupo8poo/src/main/java/com/example/grupo8poo/Grupo8pooApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo8poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo8pooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo8pooApplication.class, args);
+	}
+
+}
---
+++ grupo8poo/src/test/java/com/example/grupo8poo/Grupo8pooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo8poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo8pooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
