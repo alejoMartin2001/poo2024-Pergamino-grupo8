b1744599fe4603111b00c8c216bd88949147cd80
Enzo Villanueva <mp15265@gmail.com>
2024-10-19 17:19:28 -0300

    Se agregó el proyecto "All Music"

---
+++ allmusic/src/main/java/ar/edu/unnoba/poo2024/allmusic/AllmusicApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2024.allmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllmusicApplication.class, args);
+    }
+
+}
