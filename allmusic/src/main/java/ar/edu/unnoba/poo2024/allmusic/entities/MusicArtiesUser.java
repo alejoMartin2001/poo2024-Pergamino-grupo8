3a7a646f1f64b9e053d11a0858320c859feb870a
alejomartin2001 <amartin400@comunidad.unnoba.edu.ar>
2024-10-24 17:55:17 -0300

    actualizacion de una clase

--- allmusic/src/main/java/ar/edu/unnoba/poo2024/allmusic/entities/MusicArtiesUser.java
+++ allmusic/src/main/java/ar/edu/unnoba/poo2024/allmusic/entities/MusicArtiesUser.java
@@ -1,15 +1,12 @@
 package ar.edu.unnoba.poo2024.allmusic.entities;
 
-import java.util.List;
-
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Setter
 @Getter
-public class MusicArties extends User{
+public class MusicArtiesUser extends User{
 
     public boolean canCreateSongs(){
         return true;
