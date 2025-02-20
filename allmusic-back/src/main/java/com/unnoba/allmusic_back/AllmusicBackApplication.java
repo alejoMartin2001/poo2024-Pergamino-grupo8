596463f0fe5dd20ba3750e535e6ceb7435524c73
Enzo Villanueva <mp15265@gmail.com>
2025-02-20 18:08:48 -0300

    Creación del Back de AllMusic

---
+++ allmusic-back/src/main/java/com/unnoba/allmusic_back/AllmusicBackApplication.java
@@ -0,0 +1,13 @@
+package com.unnoba.allmusic_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllmusicBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllmusicBackApplication.class, args);
+	}
+
+}
---
+++ allmusic-back/src/test/java/com/unnoba/allmusic_back/AllmusicBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unnoba.allmusic_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllmusicBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
